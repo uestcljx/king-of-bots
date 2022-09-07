ad96bd3535fdd4b12c336d07f9643a1048b2e550
lijiangxin <531784086@qq.com>
2022-09-07 21:09:00 +0800

    项目骨架

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,26 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 李姜辛
+ * @description
+ * @since 2022/9/1 20:43
+ */
+@RestController
+@RequestMapping("/pk")
+public class BotInfoController {
+    @RequestMapping("/getbotinfo")
+    public Map<String, String> getBotInfo(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", "apple");
+        map.put("rating", "1500");
+        return map;
+
+    }
+}
---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>Bot名称：{{bot_name}}</div>
+    <div>Bot战力：{{bot_rating}}</div>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+  import {ref} from 'vue';
+  import $ from 'jquery';
+
+  export default {
+    name: "App",
+    setup: () => {
+    let bot_name = ref("")
+    let bot_rating = ref("")
+    $.ajax({
+      url:"http://localhost:3000/pk/getbotinfo",
+      type: "get",
+      success: resp => {
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating
+      }
+    })
+
+    return {bot_name, bot_rating}
+  }
+}
+  
+</script>
+
+<style>
+  body {
+    background-image: url("./assets/background.png");
+    background-size: cover
+  }
+</style>
